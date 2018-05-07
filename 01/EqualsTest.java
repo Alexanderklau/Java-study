06cc67f407e6feb96e82d67ee53023d69036b250
Alexanderklau <429095816@qq.com>
2018-05-07 17:07:14 +0800

    fix

---
+++ 01/EqualsTest.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class EqualsTest{
+    public static void main(String[] args) {
+        Employee alice1 = new Employee("Alice Adams", 75000, 1987, 12, 15);
+        Employee alice2 = alice1;
+        Employee alice3 = new Employee("Bob Brandson", 50000, 1989, 10, 1);
+
+        System.out.println()
+    }
+}
