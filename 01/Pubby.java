9243b04fbcf1aaa425c5d654e0d06d7f6693ff25
Alexanderklau <429095816@qq.com>
2019-01-03 22:49:44 +0800

    java

---
+++ 01/Pubby.java
@@ -0,0 +1,12 @@
+import java.util.*;
+/**
+ * Pubby
+ */
+public class Pubby {
+    public Pubby(String name){
+        System.out.println("Dog name is : " + name);
+    }
+    public static void main(String []args){
+        Pubby myPubby = new Pubby("tommy");
+    }
+}
